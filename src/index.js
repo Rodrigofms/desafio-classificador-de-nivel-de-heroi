778fd44d9c99006d942aeaad6ced84491e6354cd
okamigui <rodrigoms.rec@gmail.com>
2023-10-19 00:06:11 -0300

    Correção

--- src/index.js
+++ src/index.js
@@ -6,32 +6,32 @@ if (xp < 1000) {
   nome = "Kay";
   nivel = "Ferro";
 }
-else if (xp >= 1001 || xp <= 2000) {
+else if (xp >= 1001 && xp <= 2000) {
   nome = "Astra";
   nivel = "Bronze";
 }
-else if (xp >= 2001 || xp <= 5000) {
+else if (xp >= 2001 && xp <= 5000) {
   nome = "Yoru";
   nivel = "Prata";
 }
-else if (xp >= 5001 || xp <= 7000) {
+else if (xp >= 5001 && xp <= 7000) {
   nome = "Skye";
   nivel = "Ouro";
 }
-else if (xp >= 7001 || xp <= 8000) {
+else if (xp >= 7001 && xp <= 8000) {
   nome = "Breach";
   nivel = "Platina";
 }
-else if (xp >= 8001 || xp <= 9000) {
+else if (xp >= 8001 && xp <= 9000) {
   nome = "Raze";
   nivel = "Ascendente";
 }
-else if (xp >= 9001 || xp <= 10000) {
+else if (xp >= 9001 && xp <= 10000) {
   nome = "Reyna";
   nivel = "Imortal";
 }
 else {
-  nome ="Jett";
+  nome = "Jett";
   nivel = "Radiante";
 }
 
